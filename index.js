f66b8e3bb54829c154390b3c148ffaf6c8b82bcc
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-12-01 01:41:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ return document.querySelector(selector);
 function nestedTarget(){
   return document.querySelector('#nested .target');
 }
+function increaseRankBy(n){
+  const rankedLists = document.getElementbyClassName('ranked-list')
+  if(rankedLists){
+  for(let i=0,i<rankedLists.length,i++){
+    if(rankedLists[i].children){
+      for(let j=0,j<rankedLists[i].children.length,j++){
+        rankedLists[i].children[j].innerHTML = parseInt(rankedLists[i].children[j].innerHTML)+n;
+      }
+    }
+  }
+}
+}
